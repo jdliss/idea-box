151cdc4ac68b6b84f1280adfc399cba4ec50af07
Jonathan <jonacom@lissismore.com>
2016-06-27 18:19:02 -0600

    fixed formatting issue. adding idea works

--- app/assets/javascripts/add-idea.js
+++ app/assets/javascripts/add-idea.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
     $('#add-idea').submit(function(event) {
         event.preventDefault();
         var data = $(this).serialize();
-        $.post("/api/v1/ideas", data, function() {
-            var titleVal = $('#title-input').val();
-            var bodyVal = $('#body-input').val();
+        $.post("/api/v1/ideas", data, function(data) {
             $('#title-input').val('');
             $('#body-input').val('');
-            var title = "<h4><%= idea.title %></h4>";
-            var body = "<li class='truncate'>" + titleVal + "</li>";
-            var quality = "<li>" + bodyVal + "</li>";
+            var title = "<h4>" + data.title + "</h4>";
+            var body = "<li class='truncate'>" + data.body + "</li>";
+            var quality = "<li>" + data.quality + "</li>";
 
 
-            $('.ideas').prepend("<div>" + title + body + quality + "</div>");
+            $('.ideas-list').prepend("<div>" + title + body + quality + "</div>");
         });
     });
 });
