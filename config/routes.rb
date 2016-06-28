c5e88f720504b992084bb147562f94fcefebbe04
Jonathan <jonacom@lissismore.com>
2016-06-27 18:24:52 -0600

    beginning deletion. add button and class

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :ideas, only: [:create]
+      resources :ideas, only: [:create, :destroy]
     end
   end
 end
